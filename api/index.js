1cc2b8a9648256c64509f53dd1e13a7a78460237
Karan Samrat <karansamrat05039@gmail.com>
2026-09-05 12:56:19 +0530

    added mongodb and mongoose

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
+
+
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to MongoDB')
+}).catch((err)=>{
+    console.log("error in connecting to MongoDB", err)
+    console.log('issue with mongoDB server')
+})
 const app = express();
 
 app.use(express.json());
