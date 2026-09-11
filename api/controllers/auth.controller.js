fda25f76c1b43ac75e84f6b17141fc5247f4c769
Karan Samrat <karansamrat05039@gmail.com>
2026-09-11 12:05:28 +0530

    sign in API ROute created with JWT

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res,next)=>{
 
@@ -23,4 +24,22 @@ export const signup = async (req, res,next)=>{
     }
     next(error)
    }
+}
+
+export const signin = async (req, res,next)=>{
+  const {email,password} = req.body
+  try {
+    const ValidUser = await User.findOne({email:email}) // here we can also just add email cause after ES6 if value and key are same we can just add the key
+    if(!ValidUser) return next(errorHandler(404, 'User not Found'))
+      const ValidPassword = bcrypt.compareSync(password, ValidUser.password)
+    if(!ValidPassword) return next(errorHandler(420,'Wrong Credentials!'))
+      const token  = jwt.sign({id:ValidUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+    const {password:localPass, ...rest} = ValidUser._doc
+    res.cookie('access_token', token, {httpOnly: true}).status(200).json({rest})
+
+
+
+  } catch (error) {
+    next(error)
+  }
 }
